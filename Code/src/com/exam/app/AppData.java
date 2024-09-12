8e9252c67c44ffbe77e72486c59db9a3d51da9f0
0and4 <105961577+0and4@users.noreply.github.com>
2024-09-12 18:56:02 +0900

    관리자 - 상영 정보 관리
    MovieSetting.fxml / SettingController.java
    MovieSettingEdit.fxml / MovieSettingEditController.java
    AppData.java
    
    - 추가된 상영정보 레이아웃 수정 필요(HBox의 맨 왼쪽부터 채워지도록...?)
    - 추가된(저장한) 상영정보 AppData에서 관리되도록 수정 필요
    추가/수정 메뉴에서 수정한 내역 => 확인(MovieSettingCheck) 메뉴에서 모든 영화정보에 대한 상영정보를 AppData를 통해 확인할 수 있도록 수정 필요

--- Code/src/com/exam/app/AppData.java
+++ Code/src/com/exam/app/AppData.java
@@ -4,7 +4,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class AppData {
-    private static final HashMap<String, ArrayList<String>> movieScreenings = new HashMap<>();
+    private static HashMap<String, ArrayList<String>> movieScreenings = new HashMap<>();
+
+    static {
+        // 상영정보 관리에서 사용할 예시 영화 데이터 추가
+        ArrayList<String> screeningInfo1 = new ArrayList<>();
+        screeningInfo1.add("2D, 전체 관람가, 123분");
+        movieScreenings.put("Movie Title 1", screeningInfo1);
+
+        ArrayList<String> screeningInfo2 = new ArrayList<>();
+        screeningInfo2.add("4D, 15세 이상 관람가, 115분");
+        movieScreenings.put("Movie Title 2", screeningInfo2);
+    }
 
     public static HashMap<String, ArrayList<String>> getMovieScreenings() {
         return movieScreenings;
