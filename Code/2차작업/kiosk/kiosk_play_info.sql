5b2e162de91c39de6bd3e1bccf6977baca03074e
jhs1255 <happyrich15142@gmail.com>
2024-09-14 01:48:44 +0900

    DB추가

--- Code/2차작업/kiosk/kiosk_play_info.sql
+++ Code/2차작업/kiosk/kiosk_play_info.sql
@@ -18,35 +18,30 @@ USE `kiosk`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `showmovie`
+-- Table structure for table `play_info`
 --
 
-DROP TABLE IF EXISTS `showmovie`;
+DROP TABLE IF EXISTS `play_info`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `showmovie` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `play_info` (
+  `play_info_id` int NOT NULL AUTO_INCREMENT,
   `movie_id` int NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `runtime` varchar(45) NOT NULL,
-  `rating` varchar(45) NOT NULL,
-  `movietype` varchar(45) NOT NULL,
-  `poster` text,
-  PRIMARY KEY (`id`),
-  KEY `mname_idx` (`title`),
-  KEY `resitmid_idx` (`movie_id`),
-  CONSTRAINT `resitmid` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `theater_id` int NOT NULL,
+  `movie_date` date DEFAULT NULL,
+  `start_time` time DEFAULT NULL,
+  `end_time` time DEFAULT NULL,
+  PRIMARY KEY (`play_info_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `showmovie`
+-- Dumping data for table `play_info`
 --
 
-LOCK TABLES `showmovie` WRITE;
-/*!40000 ALTER TABLE `showmovie` DISABLE KEYS */;
-INSERT INTO `showmovie` VALUES (1,3,'베테랑2','118','15','2D','C:/Users/HP/JAVA/kioskproject/src/fxml/image/movie.png'),(2,3,'베테랑2','118','15','4D','C:/Users/HP/JAVA/kioskproject/src/fxml/image/movie.png'),(3,5,'트랜스포머 ONE','104','all','IMAX','C:/Users/HP/JAVA/kioskproject/src/fxml/image/movie.png'),(4,20,'탈주','94','12','3D','C:/Users/HP/JAVA/kioskproject/src/fxml/image/movie.png');
-/*!40000 ALTER TABLE `showmovie` ENABLE KEYS */;
+LOCK TABLES `play_info` WRITE;
+/*!40000 ALTER TABLE `play_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `play_info` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- Code/2차작업/kiosk/kiosk_theater.sql
+++ Code/2차작업/kiosk/kiosk_theater.sql
@@ -18,35 +18,29 @@ USE `kiosk`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `showmovie`
+-- Table structure for table `theater`
 --
 
-DROP TABLE IF EXISTS `showmovie`;
+DROP TABLE IF EXISTS `theater`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `showmovie` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `movie_id` int NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `runtime` varchar(45) NOT NULL,
-  `rating` varchar(45) NOT NULL,
-  `movietype` varchar(45) NOT NULL,
-  `poster` text,
-  PRIMARY KEY (`id`),
-  KEY `mname_idx` (`title`),
-  KEY `resitmid_idx` (`movie_id`),
-  CONSTRAINT `resitmid` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `theater` (
+  `theater_id` int NOT NULL AUTO_INCREMENT,
+  `kind` varchar(45) DEFAULT NULL,
+  `seat` int DEFAULT NULL,
+  `section` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`theater_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `showmovie`
+-- Dumping data for table `theater`
 --
 
-LOCK TABLES `showmovie` WRITE;
-/*!40000 ALTER TABLE `showmovie` DISABLE KEYS */;
-INSERT INTO `showmovie` VALUES (1,3,'베테랑2','118','15','2D','C:/Users/HP/JAVA/kioskproject/src/fxml/image/movie.png'),(2,3,'베테랑2','118','15','4D','C:/Users/HP/JAVA/kioskproject/src/fxml/image/movie.png'),(3,5,'트랜스포머 ONE','104','all','IMAX','C:/Users/HP/JAVA/kioskproject/src/fxml/image/movie.png'),(4,20,'탈주','94','12','3D','C:/Users/HP/JAVA/kioskproject/src/fxml/image/movie.png');
-/*!40000 ALTER TABLE `showmovie` ENABLE KEYS */;
+LOCK TABLES `theater` WRITE;
+/*!40000 ALTER TABLE `theater` DISABLE KEYS */;
+INSERT INTO `theater` VALUES (1,'2D',0,'1관'),(2,'2D',0,'2관'),(3,'2D',0,'3관'),(4,'3D',0,'1관'),(5,'3D',0,'2관'),(6,'3D',0,'3관'),(7,'4D',0,'3관'),(8,'4D',0,'특별관'),(9,'4DX',0,'특별관'),(10,'IMAX',0,'IMAX관');
+/*!40000 ALTER TABLE `theater` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
