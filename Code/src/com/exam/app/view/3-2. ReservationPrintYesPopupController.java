452fb8900924f7fed0ed2500f11b7b29c8722d9a
say3616 <say42481181@gmail.com>
2024-09-13 10:28:15 +0900

    예매 내역 출력 팝업

--- Code/src/com/exam/app/view/3-2. ReservationPrintYesPopupController.java
+++ Code/src/com/exam/app/view/3-2. ReservationPrintYesPopupController.java
@@ -1,6 +1,6 @@
 package com.exam.app.view;
 
-import javafx.event.ActionEvent;
+import javafx.event.ActionEvent; 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
